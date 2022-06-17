421090b4ecee13da636ae56dfe8630e9f5f54b4c
Xerewulf <58634753+Xerewulf@users.noreply.github.com>
2022-06-17 22:34:58 +0300

    Helo

---
+++ FinalGame/Assets/Sprites & Tiles/Scripts/Follow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject bat;
+    // Start is called before the first frame update
+  public PathCreator pathCreator;
+  public float speed = 3f;
+  float distanceTravelled;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position =pathCreator.path.GetPointAtDistance(distanceTravelled);
+    
+        if(bat.transform.position.y < -1.6 )
+        {
+            bat.transform.localScale = new Vector3(1,1,1);
+            
+        }
+        else if(bat.transform.position.y > -1.6)
+        {
+            bat.transform.localScale = new Vector3(-1,1,1);
+        }
+    }
+
+    
+}
