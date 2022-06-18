8ca37075b1ace9651a1b72c874444d9645bc9efe
Muhammed Bayram <muhammedbm88@gmail.com>
2022-06-18 12:25:11 +0300

    The script added for going next level with only key.

--- FinalGame/Assets/Scripts/NextLevel.cs
+++ FinalGame/Assets/Scripts/NextLevel.cs
@@ -2,14 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NextLevel : MonoBehaviour
 {
 
-    private static int numOFKey = 0;
+    public static int numOFKey = 0;
+
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        if (SceneManager.GetActiveScene().name == "Level1")
+        {
+            if(NextLevel.numOFKey == 1)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            else
+            {
+            }
+        }
+
+
         if (collision.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
